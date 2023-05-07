435f4de4eb3ef367d47b5f275588f959e665b584
TrueWinter <github@truewinter.dev>
2023-05-07 23:38:51 +0200

    fixed typo

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ wails build -platform darwin/amd64,windows/amd64 -nsis -clean
 cd ./build/bin
 pkgbuild --install-location /Applications --component Birch.app Birch.pkg
 # The pkg installer will not install to /Applications if it finds Birch.app in the build directory.
-# We don't talk about the amount of time I wasted trying troubleshoot failed installations.
+# We don't talk about the amount of time I wasted trying to troubleshoot failed installations.
 rm -r Birch.app
