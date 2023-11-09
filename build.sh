f3ee536552f40a5a68d5e1b8e27533e1240e5abb
TrueWinter <github@truewinter.dev>
2023-11-09 17:01:43 +0200

    automatically start birch after installing on mac

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ npm run test
 npm run generate-license-json
 wails build -platform darwin/amd64,windows/amd64 -nsis -clean
 cd ./build/bin
-pkgbuild --install-location /Applications --component Birch.app Birch.pkg
+pkgbuild --install-location /Applications --scripts "../darwin/scripts/" --component Birch.app Birch.pkg
 # The pkg installer will not install to /Applications if it finds Birch.app in the build directory.
 # We don't talk about the amount of time I wasted trying to troubleshoot failed installations.
 rm -r Birch.app
