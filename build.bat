b27cf53a902fb62cdf714a45f1d138eb49c16b4f
TrueWinter <nicholis@truewinter.dev>
2024-04-07 21:06:12 +0200

    fixed build script

--- build.bat
+++ build.bat
@@ -4,6 +4,6 @@ call npm run test
 call npm run generate-license-json
 call wails build -platform windows/amd64 -nsis -clean
 cd ./build/bin
-;; Only interested in the installer
+:: Only interested in the installer
 del Birch.exe
 cd ../..
