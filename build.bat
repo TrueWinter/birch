3b080a0509db775cf9c49486ed1d9fac41233765
TrueWinter <nicholis@truewinter.dev>
2024-04-07 21:04:07 +0200

    update build script

--- build.bat
+++ build.bat
@@ -5,4 +5,5 @@ call npm run generate-license-json
 call wails build -platform windows/amd64 -nsis -clean
 cd ./build/bin
 ;; Only interested in the installer
-del -r Birch-amd64.exe
+del Birch.exe
+cd ../..
